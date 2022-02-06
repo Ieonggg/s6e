c86f14243edb9402a091bcf50fb5c67a718665f5
Edward Swalker <edwardtang0611@gmail.com>
2022-02-07 00:18:46 +0800

    change app menu slide up duration

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <p>{{ isOpen }}</p>
-  <div class="fixed bottom-0 w-full h-5/6 flex flex-col justify-start items-center shadow-lg transition-all ease-in-out duration-500 " :class="isOpen ? '' : 'translate-y-[72vh]'">
+  <div class="fixed bottom-0 w-full h-5/6 flex flex-col justify-start items-center shadow-lg transition-all ease-in-out duration-300 " :class="isOpen ? '' : 'translate-y-[72vh]'">
     <button class="bg-white flex justify-center items-center w-full h-20 rounded-t-full" v-on:click="isOpen ? isOpen = false : isOpen = true">
       <div :class="isOpen ? 'hidden' : ''"> 
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
