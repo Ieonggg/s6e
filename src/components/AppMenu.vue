b0fd5c79e4e084e4f158dccb252a13b5861174f8
Edward Swalker <edwardtang0611@gmail.com>
2022-02-19 09:33:55 +0800

    refactor mini application code

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <p>{{ isOpen }}</p>
-  <div class="fixed bottom-0 w-full h-5/6 flex flex-col justify-start items-center shadow-lg transition-all ease-in-out duration-300 " :class="isOpen ? '' : 'translate-y-[72vh]'">
+  <div class="fixed bottom-0 w-full h-5/6 flex flex-col justify-start items-center shadow-lg transition-all ease-in-out duration-300 " :class="isOpen ? '' : 'translate-y-[74vh]'">
     <button class="bg-white flex justify-center items-center w-full h-20 rounded-t-full" v-on:click="isOpen ? isOpen = false : isOpen = true">
       <div :class="isOpen ? 'hidden' : ''"> 
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -13,7 +13,7 @@
         </svg>
       </div>
     </button>
-    <div class="w-full h-full bg-white flex flex-row flex-wrap justify-evenly p-5 overflow-auto">
+    <div class="w-full h-full bg-white flex flex-row flex-wrap justify-center content-start p-5 overflow-auto">
       <MiniApplication appName="值日" appDestination="Home" class="bg-gradient-to-l from-green-300 via-blue-500 to-purple-600">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"/>
       </MiniApplication>
--- src/components/MiniApplication.vue
+++ src/components/MiniApplication.vue
@@ -1,6 +1,6 @@
 <template>
-    <router-link tag="button" :to="{ name: appDestination }" class="flex flex-col justify-center items-center space-y-2 mx-2 w-32 h-32 md:w-48 md:h-48 mb-6 shadow-2xl rounded-3xl transition ease-in-out duration-500 transform hover:scale-110">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 md:h-16 w-12 md:w-16" fill="none" viewBox="0 0 24 24" stroke="white">
+    <router-link tag="button" :to="{ name: appDestination }" class="flex flex-col justify-center items-center flex-wrap md:space-y-2 mx-2 w-24 h-24 md:w-28 md:h-28 mb-6 shadow-2xl rounded-3xl transition ease-in-out duration-500 transform hover:scale-110">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 md:h-8 w-6 md:w-8" fill="none" viewBox="0 0 24 24" stroke="white">
             <slot></slot>
         </svg>
         <p class="text-white md:text-2xl font-bold md:font-normal">{{ appName }}</p>
